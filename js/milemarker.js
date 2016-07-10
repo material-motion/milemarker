25a40ae27cbd0323cdc96d014a29a1331dc988f7
Jeff Verkoeyen <featherless@google.com>
2016-07-10 18:28:40 -0400

    Show open and closed milestones.

--- js/milemarker.js
+++ js/milemarker.js
@@ -51,6 +51,7 @@ $(function() {
       } else if (repo.name.match(/-(web|js)$/)) {
         filterClass = 'tag-web';
       }
+      repo['filterClass'] = filterClass;
 
       // Fetch this repo's milestones.
 
@@ -59,52 +60,67 @@ $(function() {
         sort: 'due_on'
       }, function(milestones) {
         milestones.forEach(function(milestone) {
-          var totalIssues = milestone.closed_issues + milestone.open_issues;
+          var card = createMilestoneCard.bind(this)(milestone);
+          $('#container .open').append(card);
+        }, repo);
+      }.bind(repo));
 
-          var title = document.createElement('div');
-          var titleLink = document.createElement('a');
-          titleLink.href = "https://github.com/" + this.owner.login + "/" + this.name + "/milestone/" + milestone.number;
-          titleLink.appendChild(document.createTextNode(this.shortName + " / " + milestone.title));
-
-          title.appendChild(titleLink);
-
-          var description = document.createElement('div');
-          var descriptionHTML = md.render(milestone.description);
-          description.innerHTML = descriptionHTML;
-
-          var actions = document.createElement('div');
-
-          var links = document.createElement('div');
-          links.appendChild(document.createTextNode(milestone.open_issues + " issues remain"));
-          actions.appendChild(links);
-
-          if (totalIssues > 0) {
-            var progress = document.createElement('div');
-            progress.className = 'mdl-progress mdl-js-progress';
-            componentHandler.upgradeElement(progress);
-            progress.MaterialProgress.setProgress(milestone.closed_issues / totalIssues * 100);
-
-            actions.appendChild(progress);
-          }
-
-          var card = createCard(title, description, actions);
-          card.className += " " + filterClass;
-          $('#container').append(card);
-
-          if ('tag-' + localStorage.getItem('filter') != filterClass) {
-            $(card).hide();
-          }
+      requestGitHubAPI('/repos/material-motion/' + repo.name + '/milestones', {
+        state: 'closed',
+        sort: 'due_on'
+      }, function(milestones) {
+        milestones.forEach(function(milestone) {
+          var card = createMilestoneCard.bind(this)(milestone);
+          $('#container .closed').append(card);
         }, repo);
       }.bind(repo));
     });
   });
+
+  function createMilestoneCard(milestone, parentNode) {
+    var totalIssues = milestone.closed_issues + milestone.open_issues;
+
+    var title = document.createElement('div');
+    var titleLink = document.createElement('a');
+    titleLink.href = "https://github.com/" + this.owner.login + "/" + this.name + "/milestone/" + milestone.number;
+    titleLink.appendChild(document.createTextNode(this.shortName + " / " + milestone.title));
+
+    title.appendChild(titleLink);
+
+    var description = document.createElement('div');
+    var descriptionHTML = md.render(milestone.description);
+    description.innerHTML = descriptionHTML;
+
+    var actions = document.createElement('div');
+
+    var links = document.createElement('div');
+    var pluralText = milestone.open_issues == 1 ? "issue remains" : "issues remain";
+    links.appendChild(document.createTextNode(milestone.open_issues + " " + pluralText + " out of " + totalIssues));
+    actions.appendChild(links);
+
+    if (totalIssues > 0) {
+      var progress = document.createElement('div');
+      progress.className = 'mdl-progress mdl-js-progress';
+      componentHandler.upgradeElement(progress);
+      progress.MaterialProgress.setProgress(milestone.closed_issues / totalIssues * 100);
+
+      actions.appendChild(progress);
+    }
+
+    var card = createCard(title, description, actions);
+    card.className += " " + this.filterClass;
+    if ('tag-' + localStorage.getItem('filter') != this.filterClass) {
+      $(card).hide();
+    }
+    return card;
+  }
   
   // Utility methods
 
   // Create a material lite card.
   function createCard(titleNode, descriptionNode, actionsNode) {
     var card = document.createElement('div');
-    card.className = 'mdl-cell mdl-color--white mdl-grid mdl-card  mdl-shadow--2dp';
+    card.className = 'mdl-cell mdl-color--white mdl-grid mdl-card mdl-shadow--2dp';
     titleNode.className = 'mdl-card__title';
     descriptionNode.className = 'mdl-card__supporting-text';
     actionsNode.className = 'mdl-card__actions mdl-card--border';
