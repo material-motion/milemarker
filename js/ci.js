e308d9e714945f0bc41a3fa6d13d1bd97b0d636d
Jeff Verkoeyen <featherless@google.com>
2016-08-01 12:38:15 -0400

    Better error handling for CI.

--- js/ci.js
+++ js/ci.js
@@ -2,7 +2,7 @@ $(function() {
   var tbody = $('#container table tbody');
   requestTravisAPI('/repos/' + owner_name, {"active": "true"}, function(results) {
     results.forEach(function(repo) {
-      if (repo.last_build_status === null) {
+      if (repo.last_build_status === null && repo.last_build_started_at === null) {
         return;
       }
 
@@ -11,15 +11,18 @@ $(function() {
       var row = document.createElement('tr');
       tbody.append(row);
 
-      console.log(repo);
-
       var starNode = newStarButton(repo, "https://github.com/" + repo.slug);
       row.appendChild(newTextColumn(starNode));
       row.appendChild(newTextColumn(newHref(repo.shortName, "https://travis-ci.org/" + repo.slug)));
       
       function newBuildStatusBox(result) {
-        var icon = newIcon(result == '0' ? 'check' : 'error');
-        if (result != 0) {
+        var icon;
+        if (result == '0') {
+          icon = newIcon('check');
+        } else if (repo.last_build_status === null && repo.last_build_finished_at === null) {
+          icon = newIcon('autorenew');
+        } else {
+          icon = newIcon('error');
           icon.className = 'material-icons mdl-color-text--red-200';
         }
         return icon;
